f414ad9ffd4715ce634b97d6a201dd9c5b4116f2
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2020-06-15 14:36:52 +0530

    innitial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const puppeteer = require('puppeteer');
+
+app.get("/", async (request, response) => {
+    try {
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox']
+        });
+        const page = await browser.newPage();
+        await page.goto(request.query.url); // Read url query parameter.
+        const image = await page.screenshot({ fullPage: true });
+        await browser.close();
+        response.set('Content-Type', 'image/png');
+        response.send(image);
+    } catch (error) {
+        console.log(error);
+        response.set('Content-Type', 'application/json');
+        response.json({"error":"Please Check Your Url!"})
+    }
+});
+
+app.listen(3000);
